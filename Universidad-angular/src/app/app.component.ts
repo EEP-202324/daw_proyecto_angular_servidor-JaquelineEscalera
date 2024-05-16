bb9d73148862d227a7c2c5887ac51eabd7d78cb6
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-05-17 01:56:22 +0200

    arreglar fallos

--- Universidad-angular/src/app/app.component.ts
+++ Universidad-angular/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
--- Universidad-angular/src/app/home/home.component.ts
+++ Universidad-angular/src/app/home/home.component.ts
@@ -1,39 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UniversityLocationComponent } from '../university-location/university-location.component';
-import { UniversityLocation } from '../universitylocation';
 import { UniversityService } from '../university.service';
+import { UniversityLocation } from '../universitylocation';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [
-    CommonModule,
-    UniversityLocationComponent
-  ],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" #filter>
-        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-university-location
-        *ngFor="let universityLocation of filteredLocationList"
-        [universityLocation]="universityLocation">
-      </app-university-location>
-    </section>
-  `,
-  styleUrls: ['./home.component.css'],
+  template: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-
 export class HomeComponent {
   universityLocationList: UniversityLocation[] = [];
-  universityService: UniversityService = inject(UniversityService);
   filteredLocationList: UniversityLocation[] = [];
 
-  constructor() {
+  constructor(private universityService: UniversityService) {
     this.universityService.getAllUniversityLocations().then((universityLocationList: UniversityLocation[]) => {
       this.universityLocationList = universityLocationList;
       this.filteredLocationList = universityLocationList;
