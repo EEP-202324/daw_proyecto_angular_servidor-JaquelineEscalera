830e81b145eb6a09383d9173fd98acf9a230e943
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-05-17 01:20:14 +0200

    housing-location.component.ts y css
    casi terminados

--- Universidad-angular/src/app/housing-location/housing-location.component.ts
+++ Universidad-angular/src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UniversityLocation } from '../universitylocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-housing-location',
+  selector: 'app-university-location',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img class="listing-photo" [src]="universityLocation.photo" alt="Exterior photo of {{universityLocation.name}}">
+      <h2 class="listing-heading">{{ universityLocation.name }}</h2>
+      <p class="listing-location">{{ universityLocation.city}}, {{universityLocation.state }}</p>
+      <a [routerLink]="['/details', universityLocation.id]">Learn More</a>
+    </section>
   `,
-  styleUrl: './housing-location.component.css'
+  styleUrls: ['./housing-location.component.css'],
 })
-export class HousingLocationComponent {
+
+export class UniversityLocationComponent {
+
+  @Input() universityLocation!: UniversityLocation;
 
 }
