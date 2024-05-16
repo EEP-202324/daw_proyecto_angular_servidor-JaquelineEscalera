c3d5f530c96cbffd8719b945edf81522c3588083
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-05-16 23:26:39 +0200

    error al comitear

--- first-app-lesson-00/src/app/Ejercicio-1.py
+++
@@ -1,8 +0,0 @@
-print("programa de evaluacion de notas de alumnos")
-nota_alumno=int(input())
-def evaluacion(nota):
-	valoracion="aprobado"
-	if nota<5:
-		valoracion="suspenso"
-		return(valoracion)
-print(evaluacion(nota_alumno))
