c77ab4a10d53afd74626a1a3a07f19316cb8b759
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-05-17 01:18:03 +0200

    home.component.css y ts casi terminados
    filtre la ubicación de las universidades entre otras cosas

--- Universidad-angular/src/app/home/home.component.ts
+++ Universidad-angular/src/app/home/home.component.ts
@@ -1,16 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UniversityLocationComponent } from '../university-location/university-location.component';
+import { UniversityLocation } from '../universitylocation';
+import { UniversityService } from '../university.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    UniversityLocationComponent
+  ],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-university-location
+        *ngFor="let universityLocation of filteredLocationList"
+        [universityLocation]="universityLocation">
+      </app-university-location>
+    </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  universityLocationList: UniversityLocation[] = [];
+  universityService: UniversityService = inject(UniversityService);
+  filteredLocationList: UniversityLocation[] = [];
+
+  constructor() {
+    this.universityService.getAllUniversityLocations().then((universityLocationList: UniversityLocation[]) => {
+      this.universityLocationList = universityLocationList;
+      this.filteredLocationList = universityLocationList;
+    });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.universityLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.universityLocationList.filter(
+      universityLocation => universityLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 }
