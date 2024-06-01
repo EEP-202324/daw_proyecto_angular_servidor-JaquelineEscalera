03c900ca23176239c0a5a2853d7c9509275e5d9e
JaquelineEscalera <145449018+JaquelineEscalera@users.noreply.github.com>
2024-06-01 21:37:59 +0200

    elimine carpetas restantes

--- Universidad-spring/Universidad/src/main/java/com/example/Universidad/UniversidadRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Universidad;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UniversidadRepository extends CrudRepository<Universidad, Long> {
-}
--- Universidades-angular/src/app/universidad.ts
+++
@@ -1,6 +0,0 @@
-export interface Universidad {
-  id: number;
-  nombre: string;
-  gmail: string;
-  numero: string;
-}
